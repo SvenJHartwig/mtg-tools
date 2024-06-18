70664b32e2dc5a723fd172baa9fe93c8d8ae3a5c
sven <sven.hartwig@posteo.de>
2024-06-18 19:32:06 +0200

    use dev profile for spring test

--- src/test/kotlin/de/tholor/web/WebApplicationTests.kt
+++ src/test/kotlin/de/tholor/web/WebApplicationTests.kt
@@ -3,9 +3,11 @@ package de.tholor.web
 import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles(profiles = ["dev"])
 class WebApplicationTests {
 
     @Test
